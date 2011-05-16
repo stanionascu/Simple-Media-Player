244a5ae3f54fe80dde52a031a4cd1487f9dc9b95
Stanislav Ionascu <stanislav.ionascu@nokia.com>
2011-05-16 21:02:17 +0300

    Changes: Updated license and copyright.

--- src/smpview.h
+++ src/smpview.h
@@ -1,3 +1,23 @@
+/************************************************************************************
+** Simple Media Player, allows to watch videos with simple and uncluttered interface.
+** Copyright (C) 2011 Stanislav Ionascu
+**
+** Contact: Stanislav Ionascu <stanislav.ionascu@gmail.com>
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************************/
+
 #ifndef SMPVIEW_H
 #define SMPVIEW_H
 
